cc5546882ce083eea92b0a567976e947e4d5602a
HirushaRadeeshan <104670412+HirushaRadeeshan@users.noreply.github.com>
2022-05-04 09:06:08 +0530

    Update p32.ino

--- p32/p32.ino
+++ p32/p32.ino
@@ -1,39 +1,41 @@
 TaskHandle_t Task_Handle1;
+TaskHandle_t Task_Handle2;
 
-static const int led = LED_BUILTIN;
+static const int led = LED_BUILTIN; //selecting inbuilt LED pin
 
-void Toggle_LED1(void *parameter1){
+void Toggle_LED1(void *parameter1){   //Function of the Task1
   
-  while(1){
-    digitalWrite(led,HIGH);
-    vTaskDelay(pdMS_TO_TICKS( 500 ));
-    digitalWrite(led,LOW);
-    vTaskDelay(pdMS_TO_TICKS( 500 ));
+  while(1){                         //starting an infinity while loop
+    digitalWrite(led,HIGH);                         //light up LED
+    vTaskDelay(pdMS_TO_TICKS( 500 ));               //500ms delay
+    digitalWrite(led,LOW);                          //turn off LED
+    vTaskDelay(pdMS_TO_TICKS( 500 ));               //500ms delay
     Serial.println("Toggle LED with 500ms delay");
   } 
 }
 
-void Toggle_LED2(void *parameter2){
+void Toggle_LED2(void *parameter2){   //Function of the Task2
   
-  while(1){
-    digitalWrite(led,HIGH);
-    vTaskDelay(pdMS_TO_TICKS( 300 ));
-    digitalWrite(led,LOW);
-    vTaskDelay(pdMS_TO_TICKS( 300 ));
+  while(1){                         //starting an infinity while loop
+    digitalWrite(led,HIGH);                         //light up LED
+    vTaskDelay(pdMS_TO_TICKS( 300 ));               //300ms delay
+    digitalWrite(led,LOW);                          //turn off LED
+    vTaskDelay(pdMS_TO_TICKS( 300 ));               //300ms delay
     Serial.println("Toggle LED with 300ms delay");
   } 
 }
 
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(led,OUTPUT);
+
+  pinMode(led,OUTPUT);          //setting LED pin as output
+  //setting the Task1
   xTaskCreate(Toggle_LED1,"Toggle LED with 500ms",1024,NULL,1,&Task_Handle1);
-  xTaskCreate(Toggle_LED2,"Toggle LED with 300ms",1024,NULL,1,&Task_Handle1);
+  //setting the Task2
+  xTaskCreate(Toggle_LED2,"Toggle LED with 300ms",1024,NULL,1,&Task_Handle2);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
