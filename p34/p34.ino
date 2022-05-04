8900aa2522af5098ad64b0ec1bd8c2498ecdd5c5
HirushaRadeeshan <104670412+HirushaRadeeshan@users.noreply.github.com>
2022-05-04 09:08:49 +0530

    Update p34.ino

--- p34/p34.ino
+++ p34/p34.ino
@@ -1,40 +1,42 @@
-static const int led = LED_BUILTIN;
+static const int led = LED_BUILTIN; //selecting inbuilt LED as LED
 
-int value=0;
-char val;
-String pass;
+int Delay=100;        //creating a variable to led delay and setting it as 100ms
+char val;             //creating a variable to input value
+String pass;          //creating a variable to store new delay
 
-void toggleLED(void *parameter) {
+void LEDblink(void *parameter) {  //creating task function to blink LED 
   
-  while(1) {
-    digitalWrite(led, HIGH);
-    vTaskDelay(pdMS_TO_TICKS( value ));
-    digitalWrite(led, LOW);
-    vTaskDelay(pdMS_TO_TICKS( value ));
+  while(1) {                         //starting an infinity while loop
+    digitalWrite(led, HIGH);                         //light up LED
+    vTaskDelay(pdMS_TO_TICKS( Delay ));               //500ms delay
+    digitalWrite(led, LOW);                          //turn off LED
+    vTaskDelay(pdMS_TO_TICKS( Delay ));               //500ms delay
     Serial.print("present LED delay - ");
-    Serial.println(value);
+    Serial.println(Delay);          //printing LED delay
   }
 }
 
-void input(void *parameter){
- while(1)
- {
-  if(Serial.available()>0){   
-     val=Serial.read();
-     pass=pass+val; 
-     value=pass.toInt();     
+void input(void *parameter){  //creating task function to get input delay
+
+ while(1) {                         //starting an infinity while loop
+  if(Serial.available()>0){         //checking availability of input
+     val=Serial.read();             //storing input value in val
+     pass=pass+val;                 //putting that value to pass string
+     Delay=pass.toInt();            //converting to int and storing that value to delay
   }  
-  if(Serial.available()==0){
-     vTaskDelay(100/portTICK_PERIOD_MS);
-     pass="";
+  if(Serial.available()==0){        //checking input was not there
+     vTaskDelay(100/portTICK_PERIOD_MS);  //giving a delay
+     pass="";                       //resetting pass variable
   }
  }   
 }
 void setup() {
   Serial.begin(115200);
-  pinMode(led, OUTPUT);
-  xTaskCreate(input,"Input",1024,NULL,1,NULL);  
-  xTaskCreate(toggleLED,"Toggle",1024,NULL,1,NULL);   
+  pinMode(led, OUTPUT);          //setting LED pin as output
+  //setting the Task1
+  xTaskCreate(input,"Input",1024,NULL,1,NULL);
+  //setting the Task2
+  xTaskCreate(LEDblink,"Toggle",1024,NULL,1,NULL);   
 }
 void loop() {
 
